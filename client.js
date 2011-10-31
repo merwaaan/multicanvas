88372ccc64c3913a27857d6340168e561e3d57d9
Merwan Achibet <merwandonut@hotmail.com>
2011-10-31 10:53:40 +0100

    displayed path in real time, cleaned up

--- client.js
+++ client.js
@@ -1,126 +1,122 @@
 var drawing = false;
+
 var lastPoint = null;
-var path = [];
 
 var canvas = null;
 var ctxt = null;
 
 var myHue = null;
-var hues = {};
+var otherHues = {};
 
 var socket = null;
 
 function init() {
 
-		socket = io.connect('http://localhost');
+	 socket = io.connect('http://localhost');
 
-		socket.on('connected', function(data) {
+	 socket.on('connected', function(data) {
 
-				myHue = data.myHue;
-				hues = data.otherHues;
+		  myHue = data.myHue;
+		  otherHues = data.otherHues;
 
-				console.log('Connected with hue ' + myHue);
-		});
+		  console.log('Connected with hue ' + myHue);
+	 });
 
-		socket.on('user joins', function(data) {
+	 socket.on('user joins', function(data) {
 
-				var userId = data.id;
-				var userHue = data.hue;
+		  var userId = data.id;
+		  var userHue = data.hue;
 
-				hues[userId] = userHue;
+		  otherHues[userId] = userHue;
 
-				console.log('User ' + userId + ' joined with hue ' + userHue);
-		});
+		  console.log('User ' + userId + ' joined with hue ' + userHue);
+	 });
 
-		socket.on('user leaves', function(data) {
+	 socket.on('user leaves', function(data) {
 
-				var userId = data.id;
+		  var userId = data.id;
 
-				delete hues[userId];
+		  delete hues[userId];
 
-				console.log('User ' + userId + ' leaved');
-		});
+		  console.log('User ' + userId + ' leaved');
+	 });
 
-		socket.on('user draws', function(data) {
-				console.log(hues);
-				console.log(data);
-				var userId = data.id;
+	 socket.on('user draws', function(data) {
 
-				draw(data.path, hues[userId]);
-		});
+		  var userId = data.id;
 
-		canvas = $('canvas');
-		canvas.bind('mousedown', startDrawing);
-		canvas.bind('mouseup', stopDrawing);
-		canvas.bind('mousemove', doDrawing);
+		  draw(data.path, otherHues[userId]);
+	 });
 
-		canvas.attr('width', document.width);
-		canvas.attr('height', document.height);
+	 canvas = $('canvas');
+	 canvas.attr('width', document.width);
+	 canvas.attr('height', document.height);
+	 canvas.bind('mousedown', startDrawing);
+	 canvas.bind('mouseup', stopDrawing);
+	 canvas.bind('mousemove', doDrawing);
 
-		ctxt = canvas[0].getContext('2d');
-	ctxt.lineWidth = 10;
-
-	ctxt.lineJoin = 'round';
-	ctxt.lineCap = 'round';
-
+	 ctxt = canvas[0].getContext('2d');
+	 ctxt.lineWidth = 10;
+	 ctxt.lineJoin = 'round';
+	 ctxt.lineCap = 'round';
 }
 
 function startDrawing(event) {
 
-		drawing = true;
+	 drawing = true;
 
-		var x = event.clientX;
-		var y = event.clientY;
-
-		lastPoint = {x: x, y: y};
+	 lastPoint = {x: event.clientX, y: event.clientY};
 }
 
 function stopDrawing(event) {
 
-		drawing = false;
+	 drawing = false;
 
-		socket.emit('user draws', {
-				path: path
-		});
-
-		path = [];
-		lastPoint = null;
+	 lastPoint = null;
 }
 
 function doDrawing(event) {
 
-		if(!drawing)
-				return;
+	 if(!drawing)
+		  return;
 
-		var x = event.clientX;
-		var y = event.clientY;
+	 var currentPoint = {x: event.clientX, y: event.clientY};
 
-		path.push([x, y]);
+	 if(lastPoint !== null) {
 
-		if(lastPoint !== null) {
-			draw(path.slice(path.length-2), myHue);
-		}
+		  var path = [
+				[lastPoint.x, lastPoint.y],
+				[currentPoint.x, currentPoint.y]
+		  ];
 
-		lastPoint.x = x;
-		lastPoint.y = y;
+		  // Dessine le tracé.
+		  draw(path, myHue);
+
+		  // Envoie le tracé aux autres clients.
+		  socket.emit('user draws', {
+				path: path
+		  });
+	 }
+
+	 lastPoint = currentPoint;
 }
 
 function draw(path, hue) {
 
-		ctxt.strokeStyle = 'hsl(' + hue + ',60%,50%)';
-		console.log(hue);
+	 ctxt.strokeStyle = 'hsl(' + hue + ',60%,50%)';
+	 console.log(hue);
 
-		ctxt.beginPath();
-		ctxt.moveTo(path[0][0], path[0][1]);
+	 ctxt.beginPath();
+	 ctxt.moveTo(path[0][0], path[0][1]);
 
-		for(var i = 1; i < path.length; ++i)
-				ctxt.lineTo(path[i][0], path[i][1]);
+	 for(var i = 1; i < path.length; ++i)
+		  ctxt.lineTo(path[i][0], path[i][1]);
 
 
-		ctxt.stroke();
+	 ctxt.stroke();
 }
 
 $(function() {
 
-		init();
+	 init();
 });
